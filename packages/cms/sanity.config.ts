7960111dc87c46f44a722f101856f8df9c96baf0
Bram Smulders <source@bram.is>
2023-09-07 18:49:43 +0200

    install sanity studio in CMS package

---
+++ packages/cms/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'bram.is homepage',
+
+  projectId: 'a0nc81hk',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
