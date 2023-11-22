88eae90cee47600f4d7296cb78fffde9ee5e6a53
Bram Smulders <source@bram.is>
2023-11-22 15:14:25 +0100

    Update packages, Fix for build issue from @sanity/astro

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import { loadEnv } from 'vite';
-import sanity from '@sanity/astro';
+import { sanityIntegration as sanity } from '@sanity/astro';
 
 const { PUBLIC_SANITY_STUDIO_PROJECT_ID, PUBLIC_SANITY_STUDIO_DATASET } =
 	loadEnv(import.meta.env.MODE, process.cwd(), '');
